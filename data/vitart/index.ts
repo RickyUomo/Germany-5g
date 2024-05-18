f09312dcc52d76f082bb4b052262fbd8520f5b78
personal <rickykuo925@gmail.com>
2024-05-18 20:20:34 +0100

    test

--- data/vitart/index.ts
+++ data/vitart/index.ts
@@ -1,28 +1,13 @@
 const VITART = [
   {
-    name: "hello world",
-    href: "123",
+    name: "VR 1",
+    href: "/quicklook/quick1.reality",
     image: "/example/product4.png",
   },
   {
-    name: "hello world",
-    href: "123",
-    image: "/example/product4.png",
-  },
-  {
-    name: "hello world",
-    href: "123",
-    image: "/example/product4.png",
-  },
-  {
-    name: "hello world",
-    href: "123",
-    image: "/example/product4.png",
-  },
-  {
-    name: "hello world",
-    href: "123",
-    image: "/example/product4.png",
+    name: "VR 2",
+    href: "/quicklook/quick2.reality",
+    image: "/example/product1.png",
   },
 ];
 
