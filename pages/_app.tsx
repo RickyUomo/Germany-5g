446e0e9255bd47438d56a72175e01c3bde5f0177
personal <rickykuo925@gmail.com>
2024-05-22 10:59:36 +0100

    add google ana

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,14 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 import { Layout } from "@/components/Layout";
+import { GoogleTagManager } from "@next/third-parties/google";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
       <Layout>
         <Component {...pageProps} />
+        <GoogleTagManager gtmId="G-0DYS93CCM3" />
       </Layout>
     </ChakraProvider>
   );
